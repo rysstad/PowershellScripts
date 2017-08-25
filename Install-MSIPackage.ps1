35285d6cd311dbcc3ddee48ec18e479f029af46c
Tor Arne Rysstad <rysstad2@gmail.com>
2017-08-25 09:25:46 +0200

    Delete Install-MSIPackage.ps1

--- Install-MSIPackage.ps1
+++
@@ -1,4 +0,0 @@
-$msiPackageToInstall = "<path to .msi>"
-# save log from installation to temp file 
-$tmpFile = [System.IO.Path]::GetTempFileName()
-Start-Process msiexec -ArgumentList "/i $msiPackageToInstall /qn /L*v $tmpFile" -NoNewWindow -Wait
